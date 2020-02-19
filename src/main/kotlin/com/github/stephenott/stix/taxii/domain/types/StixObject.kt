a57c260ef359922c49e531b1db4ba6d946420ac1
StephenOTT <stephen@digitalstate.ca>
2020-02-19 18:33:24 -0500

    add support to ensure that the stix object internal storage is not exposed

--- src/main/kotlin/com/github/stephenott/stix/taxii/domain/types/StixObject.kt
+++ src/main/kotlin/com/github/stephenott/stix/taxii/domain/types/StixObject.kt
@@ -2,12 +2,14 @@ package com.github.stephenott.stix.taxii.domain.types
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter
 import com.fasterxml.jackson.annotation.JsonAnySetter
+import io.swagger.v3.oas.annotations.Hidden
 import io.swagger.v3.oas.annotations.media.Schema
 
 //@TODO need to move to a model converter
-@Schema(name = "stix-object", description = "A STIX Object")
+@Schema(name = "stix-object", description = "A STIX Object", type = "object")
 data class StixObject(
         @JsonAnySetter
         @get:JsonAnyGetter
+        @field:Hidden
         val stixObject: Map<String, Any> = mutableMapOf() //@TODO mapof()
 )
